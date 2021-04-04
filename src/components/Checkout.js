a6d3a66d284e60a5ddcd7a1276377e13adaac9c6
Mike Nieva <m@mikenieva.com>
2021-04-03 19:27:31 -0600

    Final

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -15,10 +15,7 @@ const Checkout = () => {
               <h3 class="text-gray-600">Total de la orden: </h3>
             </div>
 
-            <button
-              type="button"
-              class="w-full items-center px-6 py-3 mt-10 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
+            <button type="button" class="btn-indigo">
               Pagar
             </button>
           </div>
